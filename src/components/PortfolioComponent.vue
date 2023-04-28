c38e5abcc44e17468c6d3b102799393dc6e25f76
jlucartc <jlucartc@gmail.com>
2023-04-28 00:15:44 -0300

    Fixing logic

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -39,7 +39,7 @@
         .then(data => {console.log('data: ',data); return data})
         .then(data => {tags.value = data.tags; return data})
         .then(data => {
-            data.tags = data.tags.splice(1,0,{id: 0, name: 'Any'})
+            data.tags.splice(1,0,{id: 0, name: 'Any'})
             let message = {tags: [data.tags[0].id]}
             return search_projects(message)
         })
